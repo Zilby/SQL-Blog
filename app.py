4835e342d5d15bbe8c0df22350317a7088bd4c14
Andrew Sikowitz <nysiko@gmail.com>
2014-10-21 11:52:27 -0400

    Doesn't work

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
+conn = sqlite3.connect("data.db")
+c = conn.cursor()
+#q = "create table blogs(title text, body text)"
+#c.execute(q)
+#conn.commit()
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def home():
+    title = request.args.get("title", None)
+    body = request.args.get("body", None)
+    print title
+    if (title != None and body != None):
+        print "GOOD"
+        q = "INSERT INTO blogs VALUES(%s, %s)" % (title, body)
+        c.execute(q)
+        conn.commit()
+        q = "select * from blogs"
+        result = c.execute(q)
+        print result
     L = ["One", "two"]
     return render_template("home.html", l = L)
     
