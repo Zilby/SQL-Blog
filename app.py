d1274744344f5b03d2085af2c533355dccc30c60
asikowitz <nysiko@gmail.com>
2014-10-21 21:54:29 -0400

    Backbone of website

--- app.py
+++ app.py
@@ -1,37 +1,53 @@
-#!/usr/bin/python
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 import sqlite3
 
 app = Flask(__name__)
-conn = sqlite3.connect("data.db")
-c = conn.cursor()
-#q = "create table blogs(title text, body text)"
-#c.execute(q)
-#conn.commit()
+DATABASE = 'data.db'
+
+#Help from http://flask.pocoo.org/docs/0.10/patterns/sqlite3/
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect('data.db')
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route("/", methods=["GET","POST"])
 def home():
-    title = request.args.get("title", None)
-    body = request.args.get("body", None)
-    print title
-    if (title != None and body != None):
-        print "GOOD"
-        q = "INSERT INTO blogs VALUES(%s, %s)" % (title, body)
+    g.conn = get_db()
+    c = g.conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS blogs(title text, body text)")
+    g.conn.commit()
+    if ('title' in request.form and 'body' in request.form):
+        title = request.form['title']
+        body = request.form['body']
+        q = "INSERT INTO blogs VALUES('%s', '%s')" % (title, body)
         c.execute(q)
-        conn.commit()
+        g.conn.commit()
         q = "select * from blogs"
         result = c.execute(q)
-        print result
+        for r in result:
+            print r
     L = ["One", "two"]
+    g.conn.close()
     return render_template("home.html", l = L)
     
-"""@app.route("/<name>")
-def pages(name = None):
-    #index = 
-    return render_template()
-"""
+@app.route("/<title>")
+def pages(title):
+    g.conn = get_db()
+    c = g.conn.cursor()
+    q = "select * from blogs where title == '%s'" % title#.replace("%20", " ")
+    result = c.execute(q).fetchone()
+    title = result[0]
+    body = result[1]
+    return render_template("post.html", title=title, body=body)
 
-if __name__=="__main__":
+if __name__=="__main__":    
     app.debug=True
-    app.run(host="127.0.0.1",port=5000)
-    app.run()
+    app.run(host="0.0.0.0",port=5000)
+
